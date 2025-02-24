ccae0d37a5ee0e3326d223ffa719eed910df98ea
Rill <160830457+chappuis13@users.noreply.github.com>
2025-02-24 18:24:34 +0700

    Update Task.py

--- Task 1/Task.py
+++ Task 1/Task.py
@@ -1,23 +1,15 @@
-# define class car
 class Car:
-    # initialize the Car object with brand, model, and year attributes
     def __init__(self, brand, model, year):
         self.brand = brand 
         self.model = model
-        self.year = year
+        self.year = int(year)
         
-    # define a method to display the car's information
     def display_info(self):
-        print(f"Brand: {self.brand} {self.model} - Year: {self.year}")
-        
-# example list of Car objects with different brands, models, and years
-example_car = [
-    Car("Toyota", "Corolla", 2019),
-    Car("Honda", "Civic", 2020),
-    Car("Ford", "Mustang", 2021),
-]
+        print(f'Brand: {self.brand}, Model: {self.model}, Year: {self.year}')
 
-# iterate over each Car object in the example_car list
-for car in example_car:
-    # Call the display_info method to print the car's information
-    car.display_info()
+
+car1 = Car('Honda', 'Civic', 2020)
+car2 = Car('Toyota', 'Supra', 2022)
+car3 = Car('Toyota', 'Kijang Innova', 2023)
+
+    car3.display_info()
